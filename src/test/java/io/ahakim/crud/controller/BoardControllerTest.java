7135115d8ace86edcd3b7741f804670199e7dc5e
may54ther <aha9654@gmail.com>
2022-07-23 19:19:27 +0900

    Test: 서비스 테스트 진행

--- src/test/java/io/ahakim/crud/controller/BoardControllerTest.java
+++ src/test/java/io/ahakim/crud/controller/BoardControllerTest.java
@@ -1,4 +1,57 @@
-import static org.junit.jupiter.api.Assertions.*;
+package io.ahakim.crud.controller;
+
+import io.ahakim.crud.domain.Board;
+import io.ahakim.crud.service.BoardService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
 class BoardControllerTest {
-  
+
+    private final BoardService boardService;
+    Board board = new Board();
+
+    @Autowired
+    BoardControllerTest(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @Test
+    void total() throws Exception {
+        boardService.total();
+    }
+
+    @Test
+    void getPostList() throws Exception {
+        List<Board> boardList = boardService.getPostList(1, 10);
+    }
+
+    @Test
+    void getPost() throws Exception {
+        Board board = boardService.getPost(4);
+    }
+
+    @Test
+    void addPost() throws Exception {
+        boardService.addPost(board);
+    }
+
+    @Test
+    void editPost() throws Exception {
+
+        Board editBoard = new Board();
+        editBoard.setBoardNo(9L);
+        editBoard.setTitle("수정-제목9");
+        editBoard.setContent("수정-내용9");
+
+        boardService.editPost(editBoard);
+    }
+
+    @Test
+    void removePost() throws Exception {
+        boardService.removePost(1);
+    }
 }
